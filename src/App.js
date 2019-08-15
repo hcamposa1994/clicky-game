e1c3f68ace96eb90d66e8d43e13bbc48379d9fe8
Herbert Campos <wohoforever@yahoo.com>
2019-08-14 21:44:16 -0700

    created the handle click function but so far does not reset the clicked variable upon a gameover

--- src/App.js
+++ src/App.js
@@ -7,12 +7,58 @@ import dinos from "./dinos.json"
 class App extends Component {
   state = {
     message: "Click any image to start",
-    dinos: dinos,
+    dinos,
     score: 0,
     topscore: 0,
-    alreadyClicked: []
   }
 
+  handleClick = (id) => {
+    const dinos = this.state.dinos
+    const clickedDino = dinos.filter(dino => dino.id === id)
+
+    if(clickedDino[0].clicked) {
+      for (let i = 0; i > dinos.length; i++) {
+        dinos[i].clicked = false;
+      }
+      this.setState({
+        message: "You already clicked that one, now you have to start over",
+        score: 0,
+        dinos: dinos
+      })
+    }
+    else if (this.state.topscore < 6) {
+      clickedDino[0].clicked = true
+
+      dinos.sort(function(a,b){return 0.5 - Math.random()})
+
+      this.setState({
+        dinos: dinos,
+        score: this.state.score + 1,
+        message: "Great, that one was not clicked on, keep going!"
+      }, () => {
+        if(this.state.score > this.state.topscore) {
+          this.setState({
+            topscore: this.state.score
+          })
+        }
+      })
+    }
+    else {
+      clickedDino[0].clicked = true
+
+      for(let i = 0; i < dinos.length; i++) {
+        dinos[i].clicked = false;
+      }
+      dinos.sort(function(a,b){return 0.5 - Math.random()})
+
+      this.setState({
+        dinos: dinos,
+        score: 0,
+        topscore: 6,
+        message: "CONGRADULATIONS!, now try doing that again"
+      })
+    }
+  }
 
   render() {
     return (
@@ -22,6 +68,7 @@ class App extends Component {
           <Header />
           {this.state.dinos.map(dino => (
             <Card
+              handleClick={this.handleClick}
               id={dino.id}
               key={dino.id}
               name={dino.name}
