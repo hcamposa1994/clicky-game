3d3e04029d825f98856f343243ddfa22cc1a50f0
Herbert Campos <wohoforever@yahoo.com>
2019-08-15 14:57:42 -0700

    Updates

---
+++ precache-manifest.a36ece494deed914afcb637daa4d7e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19636e12735f7d003b489b543a2f479",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "0e5240500f78daf2382c",
+    "url": "/clicky-game/static/css/main.5809f02f.chunk.css"
+  },
+  {
+    "revision": "2c85d32550131ae6d666",
+    "url": "/clicky-game/static/js/2.d048d323.chunk.js"
+  },
+  {
+    "revision": "0e5240500f78daf2382c",
+    "url": "/clicky-game/static/js/main.f718dddf.chunk.js"
+  },
+  {
+    "revision": "e900226b6c6e02320770",
+    "url": "/clicky-game/static/js/runtime~main.98b6a1fe.js"
+  }
+]);
